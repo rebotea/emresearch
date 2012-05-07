0b3d835380699c64558b93bb23fb5422d86052cf
Ewald.lp@gmail.com <Ewald.lp@gmail.com@5942b0ba-b509-359f-d79d-246bf6927bd7>
2012-05-07 09:12:58 +0000

    Исправление ошибок PMGeneratorWaveformUnit.pas

--- trunk/PMGeneratorWaveformUnit.pas
+++ trunk/PMGeneratorWaveformUnit.pas
@@ -1,5 +1,11 @@
-unit PMGeneratorWaveformUnit;
+﻿unit PMGeneratorWaveformUnit;
 
+{
+Unit calculates  and outputs different waveforms,
+calculates THD, and computes  the harmonic content
+for permanent magnet machines with  surface magnets and
+slotted stators.
+}
 
 interface
 
@@ -7,40 +13,97 @@ uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, TeEngine, Series, StdCtrls, ExtCtrls, TeeProcs, Chart, math;
 
-type TVector = array of Extended;
+type
+  TVector = array of Extended;
+
+type
+  AppendixG = class(TObject)
+  private
+    // General variables
+    // Основне переменные
+    Pwr{Required power(W)}, rpm{Speed(RPM)}, psi{Power factor angle}, f{Electrical frequency(Hz)},
+    omega{electrical frequency(rad/sec)}, vtip{Tip speed(m/s)}, lambda{Flux linkage},
+    Ea{RMS Internal voltage(V)},
+    // Rotor variables
+    // Переменные параметров ротора
+    R{Rotor radius(m)}, hm{Magnete thickness(m)}, Lst{Rotor stack length(m)},
+    p{Number of pole pairs}, Br{Magnet remnant flux density(T)}, thm{Magnet physical angle(deg)},
+    thsk{Magnet skew angle(actual deg)},
+    // Stator variables
+    // Переменные параметров статора
+    q{Number of phases}, m{Slots per pole per phase}, Ns{Number of slots}, Nsp{Nyumber of slots short pitched},
+    g{Air gap(m)}, ge{Effective air gap(m)}, tfrac{Peripheral tooth fraction},
+    hs{Slot depth(m)}, hd{Slot depression depth(m)}, wd{Slot depression width(m)},
+    syrat{Stator back iron ratio(yoke thick/rotor radius)}, Nc{Turns per coil},
+    lams{Slot fill fraction}, sigst{Stator conductivity}, Kc{Carter coefficient} : Extended;
+
+    n, np, w, freq, kgn, kwn, ksn, Eanorm, Ean, Bout, Eaout, Bn: Tvector;
+    Rs,  THD: Extended;
+
+    function VectorSizing(V: TVector): TVector;
+    function AProgressive(x, s, sum: Extended): TVector;
+    function VMul(x: Extended; V: TVector): Tvector;
+    function VMul1(x, V: TVector): Tvector;
+    function VDiv(x: Extended; V: TVector): TVector;
+    function VDiv1(x, V: TVector): TVector;
+    function VPow(x: Extended; V: TVector): TVector;
+    function VPow1(x, V: TVector): TVector;
+    function VMinus(x: Extended; V: TVector): TVector;
+    function VMinus1(x, V: TVector): TVector;
+    function VPlus(x: Extended; V: TVector): TVector;
+    function VPlus1(x, V: TVector): TVector;
+    function VSin(V: TVector): TVector;
+    function VAbs(V: TVector): TVector;
+    function SetLengthZero(Size: Integer): TVector;
+  protected
+    procedure EvaluatedToHarmonics(var n, np, w, freq: TVector);
+    procedure HarmonicWind_SkFact(Nsct, Nsfp: Extended);
+    function CalcMgapFtrs(np: TVector): TVector;
+    function CalcMFlx_InV(Bg, Na: Extended): HRESULT;
+    function Normalized(): HRESULT;
+    function GenWave(): HRESULT;
 
 
-function AProgressive(x, s, sum: Extended): TVector;
+  public
+    Constructor Create;
+    Destructor Destroy; virtual;
+    property GetReqPower: Extended read Pwr;
+    function SetGeneralVariable(ReqPower, Speed, PowFAngle, El_f_Hz, El_f_Rps, TSpeed, FlLink, RMSInV: Extended): HRESULT;
+    function PlotWaveforms(CLine_1Sr, CLine_1Sr2, CBar_2Sr: TChart): HRESULT;
+end;
 
-function VectorMul(x: Extended; V: TVector): Tvector;
+const
+  mu0 = 4 * Pi * 0.0000001;// Free space permeability
+  tol = 0.01;// Toleracne factor
 
-
+{ AppendixG }
 implementation
 
-function VectorSizing(V: TVector): TVector;
+function AppendixG.VectorSizing(V: TVector): TVector;
 begin
   while V[Length(V) - 1] = 0 do
    SetLength(V, Length(V) - 1);
   Result:= V;
 end;
 
-function AProgressive(x, s, sum: Extended): TVector;
+function AppendixG.AProgressive(x, s, sum: Extended): TVector;
 var
   i: Integer;
   Cx: Extended;
+  Result1: TVector;
 begin
   i:= 0;
-  SetLength(Result, Round((abs(sum) - abs(x)) / s + 2));
+  SetLength(Result1, Round((abs(sum) - abs(x)) / s + 2));
   while (sum >= RoundTo(x, -4)) do
   begin
-    Result[i]:= x;
+    Result1[i]:= x;
     x:= x + s;
     inc(i);
   end;
-  Result:= VectorSizing(Result);
+  Result1:= VectorSizing(Result1);
 end;
 
-function VMul(x: Extended; V: TVector): Tvector;
+function AppendixG.VMul(x: Extended; V: TVector): Tvector;
 var
   i: Integer;
 begin
@@ -53,7 +116,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VMul1(x, V: TVector): Tvector;
+function AppendixG.VMul1(x, V: TVector): Tvector;
 var
   i: Integer;
 begin
@@ -66,7 +129,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VDiv(x: Extended; V: TVector): TVector;
+function AppendixG.VDiv(x: Extended; V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -79,7 +142,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VDiv1(x, V: TVector): TVector;
+function AppendixG.VDiv1(x, V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -92,7 +155,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VPow(x: Extended; V: TVector): TVector;
+function AppendixG.VPow(x: Extended; V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -105,7 +168,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VPow1(x, V: TVector): TVector;
+function AppendixG.VPow1(x, V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -118,7 +181,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VMinus(x: Extended; V: TVector): TVector;
+function AppendixG.VMinus(x: Extended; V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -131,7 +194,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VMinus1(x, V: TVector): TVector;
+function AppendixG.VMinus1(x, V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -144,7 +207,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VPlus(x: Extended; V: TVector): TVector;
+function AppendixG.VPlus(x: Extended; V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -157,7 +220,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VPlus1(x, V: TVector): TVector;
+function AppendixG.VPlus1(x, V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -170,7 +233,7 @@ begin
   Result:= VectorSizing(Result);
 end;
 
-function VSin(V: TVector): TVector;
+function AppendixG.VSin(V: TVector): TVector;
 var
   i: Integer;
 begin
@@ -182,18 +245,59 @@ begin
   Result:= VectorSizing(Result);
 end;
 
+function AppendixG.VAbs(V: TVector): TVector;
+var
+  i: Integer;
+begin
+  i:= 0;
+  repeat
+    Result[i]:= abs(V[i]);
+    inc(i);
+  until i = Length(V);
+  Result:= VectorSizing(Result);
+end;
+
+function AppendixG.SetGeneralVariable(ReqPower, Speed, PowFAngle, El_f_Hz,
+  El_f_Rps, TSpeed, FlLink, RMSInV: Extended): HRESULT;
+begin
+  Pwr:= ReqPower;
+  rpm:= Speed;
+  psi:= PowFAngle;
+  f:= El_f_Hz;
+  omega:= El_f_Rps;
+  vtip:= TSpeed;
+  lambda:= FlLink;
+  Ea:= RMSInV;
+end;
+
+function AppendixG.SetLengthZero(Size: Integer): TVector;
+var
+  Massive: TVector;
+  I: Integer;
+begin
+  SetLength(Massive, Size);
+  for I := 0 to Length(Massive) do
+   Massive[I]:= 0;
+  Result:= Massive;
+end;
+
 
 // Harmonics  to be evaluated
-procedure EvaluatedToHarmonics(var n, np, w, freq: TVector);
+// Оценка гармоники
+procedure AppendixG.EvaluatedToHarmonics(var n, np, w, freq: TVector);
 begin
   n:= AProgressive(1, 2, 35);
-  np:= VMul(p, n);// ����������� � kgn
-  w:= VMul(omega, n);// ������������� ���������� �������
-  freq:= VDiv(2 * Pi, w);// ������������� �������
+  np:= VMul(p, n);
+  w:= VMul(omega, n);
+  freq:= VDiv(2 * Pi, w);
 end;
 
 // Harmonic  winding  and  skew factors
-procedure HarmonicWind_SkFact(n: TVector);
+// Обмотка и коэффицинт скольжения
+procedure AppendixG.HarmonicWind_SkFact(Nsct, Nsfp: Extended);
+var
+  gama, alfa, ths: Extended;
+  kpn, kbn: TVector;
 begin
   gama:= 2 * Pi * p / Ns;
   alfa:= Pi * Nsct / Nsfp;
@@ -205,51 +309,380 @@ begin
 end;
 
 // Calculate  magnetic  gap  factor
-function CalcMgapFtrs: TVector;
+// Расчет коэффициента разрыва
+function AppendixG.CalcMgapFtrs(np: TVector): TVector;
+var
+  Ri, R1, R2: Extended;
 begin
   Rs:= R + hm + g;
   Ri:= R;
   R1:= R;
   R2:= R + hm;
-  kgm:= Vmul1(VDiv1(VPow(Ri, VMinus(1, np)), VMinus1(VPow(Rs, VMul(2, np)), VPow(Ri, VMul(2, np)))),
-               )    ;
-
+  kgn:=
   VPlus1
   (
-    VMul1
+    Vmul1
     (
-      VDiv1(np, VPlus(1, np))
-      ,
-      VMinus1(VPow(R2, VPlus(1, np)),VPow(R1, VPlus(1, np))
+      Vmul1
+      (
+        VDiv1
+        (
+          VPow
+          (
+            Ri
+            ,
+            VMinus
+            (
+              1
+              ,
+              np
+            )
+          )
+          ,
+          VMinus1
+          (
+            VPow
+            (
+              Rs
+              ,
+              VMul
+              (
+                2
+                ,
+                np
+              )
+            )
+            ,
+            VPow
+            (
+              Ri
+              ,
+              VMul
+              (
+                2
+                ,
+                np
+              )
+            )
+          )
+        )
+        ,
+        VDiv1
+        (
+          np
+          ,
+          VPlus
+          (
+            1
+            ,
+            np
+          )
+        )
+      )
+    ,
+      VMinus1
+      (
+        VPow
+        (
+          R2
+          ,
+          VPlus
+          (
+            1
+            ,
+            np
+          )
+        )
+        ,
+        VPow
+        (
+          R1
+          ,
+          VPlus(1, np)
+        )
+      )
     )
-  ,
+    ,
     VMul1
     (
       VDiv1
       (
-      VPow1(VMul(Rs, np), VMul(2, np))
-      ,
-      VMinus(1, np)
-      )
-    ,
-      VMinus1
+        VPow1
         (
-         VPov
-        ,
-
+          VMul
+          (
+            Rs
+            ,
+            np
+          )
+          ,
+          Vmul
+          (
+            2
+            ,
+            np
+          )
         )
-
+        ,
+        VMinus
+        (
+          1
+          ,
+          np
+        )
+      )
+      ,
+      VMinus1
+      (
+        VPow
+        (
+          R1
+          ,
+          VMinus
+          (
+            1
+            ,
+            np
+          )
+        )
+        ,
+        VPow
+        (
+          R2
+          ,
+          VMinus
+          (
+            1
+            ,
+            np
+          )
+        )
+      )
     )
+  );
+  Result:= kgn;
+end;
+
+constructor AppendixG.Create;
+begin
+  inherited Create;
+end;
+
+destructor AppendixG.Destroy;
+begin
+  inherited Destroy;
+end;
+
+//Calculate magnetic flux and internal voltage
+//Расчет магнитного потока и внутреннего напряжения
+function AppendixG.CalcMFlx_InV(Bg, Na: Extended): HRESULT;
+var
+  thmrad, thmerad: Extended;
+  lambdan: TVector;
+begin
+  thmrad:= thm * (Pi / 180);
+  thmerad:= p * thmrad;
+  Bn:=
+  VMul1
+  (
+    VMul1
+    (
+      VMul1
+      (
+        VMul
+        (
+          Bg
+          ,
+          VDiv
+          (
+            4 / Pi
+            ,
+            n
+          )
+        )
+        ,
+        kgn
+      )
+      ,
+      VSin
+      (
+        VMul
+        (
+          thmerad / 2
+          ,
+          n
+        )
+      )
+    )
+    ,
+    VSin
+    (
+      VMul
+      (
+         Pi / 2
+         ,
+         n
+      )
+    )
+  );
+
+  lambdan:=
+  VDiv
+  (
+    p
+    ,
+    VMul1
+    (
+      VMul1
+      (
+        VMul
+        (
+          2 * Rs * Lst * Na
+          ,
+          kwn
+        )
+        ,
+        ksn
+       )
+       ,
+       Bn
+     )
+  );
+
+  // Normalized values for plotting
+  // Нормализация величин для построения графика
+  Ean:= VMul(omega, lambdan);
+  Eanorm:= VDiv(Ean[1], VAbs(Ean));
+end;
+
+// Voltage THD
+// Напряжение THD
+function AppendixG.Normalized(): HRESULT;
+var
+  r: Integer;
+  Eah: Extended;
+begin
+  Eah:= 0;
+  for r := 2 to Length(n) do
+    Eah:= Eah + sqr(Ean[r]);
+  THD:= 100 * sqrt(Eah / sqr(Ean[1]));
+end;
+
+// Generate waveforms
+// Rotor physical angle goes from 0 to 2*pi - electrical to 2*p*pi
+// Составление массива для построение волны
+// при движении ротора от 0 до 2 * Pi - в состоянии готовности до 2 * p * PI
+function AppendixG.GenWave(): HRESULT;
+var
+  ang: Extended;
+  i: Integer;
+  angp: TVector;
+begin
+  ang:= 0;
+  i:= 0;
+  SetLength(angp, Round(2 * Pi / 100) + 1);
+  Bout:= SetLengthZero(Round(2 * Pi / 100) + 1);
+  Eaout:= SetLengthZero(Round(2 * Pi / 100) + 1);
+  while ang <= 2 * Pi do
+  begin
+    ang:= ang + Pi / 100;
+    angp[i]:= p * ang;
+    inc(i);
+  end;
+  for I := 0 to Length(Bout) do
+  begin
+    Bout:=
+    VPlus1
+    (
+      Bout
+      ,
+      VMul
+      (
+        Bn[i]
+        ,
+        VSin
+        (
+          VMul
+          (
+            n[i]
+            ,
+            angp
+          )
+        )
+      )
+    );
+    VPlus1
+    (
+      Eaout
+      ,
+      VMul
+      (
+        Ean[i]
+        ,
+        VSin
+        (
+          VMul
+          (
+            n[i]
+            ,
+            angp
+          )
+        )
+      )
+    );
+  end;
+end;
+
+//Plot waveforms
+//Изображение синусоиды на графике
+
+function AppendixG.PlotWaveforms(CLine_1Sr, CLine_1Sr2, CBar_2Sr: TChart): HRESULT;
+var
+  I: Integer;
+  ang: Extended;
+begin
+  ang:= 0;
+  I:= 0;
+  CLine_1Sr.Series[0].Clear;
+  CLine_1Sr2.Series[0].Clear;
+  CBar_2Sr.Series[0].Clear;
+  CBar_2Sr.Series[1].Clear;
+  CLine_1Sr.Title.Caption:= 'PM  Generator:  Flux  Density';
+  CLine_1Sr.BottomAxis.Title.Caption:='Rotor Angle (rad)';
+  CLine_1Sr.LeftAxis.Title.Caption:='B (Tesla)';
+  CLine_1Sr2.Title.Caption:= 'PM  Generator:  Back EMF';
+  CLine_1Sr2.BottomAxis.Title.Caption:='Rotor  Angle  (rad)';
+  CLine_1Sr2.LeftAxis.Title.Caption:='Peak  Voltage  (V)';
+  CBar_2Sr.Title.Caption:= 'PM  Generator:  EMF Harmonics,  THD';
+  CBar_2Sr.BottomAxis.Title.Caption:='Harmonic  Number';
+  CBar_2Sr.LeftAxis.Title.Caption:='Normalized  Back EMF';
+  while ang < (2 * Pi / 100) do
+  begin
+    CLine_1Sr.Series[0].AddXY(ang, Bout[I]);
+    CLine_1Sr2.Series[0].AddXY(ang, Eaout[I]);
+    ang:= ang + Pi / 100;
+    inc(I);
+  end;
+
+  for I := 1 to Length(Eanorm) do
+  begin
+    if Eanorm[I] < 0.1 then
+      CBar_2Sr.Series[0].AddXY(n[I], Eanorm[I])
+    else
+      CBar_2Sr.Series[1].AddXY(n[I], Eanorm[I])
+  end;
+
+  CLine_1Sr.Refresh;
+  CLine_1Sr2.Refresh;
+  CBar_2Sr.Refresh;
 
 
 
-
-  )
-  ;
-
-
-  kgm:= Vdiv1(Vpow(Ri, VMinus(1, np)), ())      (VPow())
 end;
 
 
+
+
+
 end.
+
